0d9fca2c877df0142773597c3c63c336d122a5d1
Tiger Wang <wanghu-520@163.com>
2019-07-06 14:42:35 +0800

    1. 优化Conditional自动配置 2. 添加Conditional配置单元测试

--- guerlab-sms-aliyun-starter/src/main/java/net/guerlab/sms/aliyun/AliyunAutoConfigure.java
+++ guerlab-sms-aliyun-starter/src/main/java/net/guerlab/sms/aliyun/AliyunAutoConfigure.java
@@ -1,17 +1,11 @@
 package net.guerlab.sms.aliyun;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import net.guerlab.sms.aliyun.AliyunAutoConfigure.EnableCondition;
 
 /**
  * 阿里云发送端点自动配置
@@ -20,7 +14,7 @@ import net.guerlab.sms.aliyun.AliyunAutoConfigure.EnableCondition;
  *
  */
 @Configuration
-@Conditional(EnableCondition.class)
+@ConditionalOnProperty(prefix = "sms.aliyun", name = "enable", havingValue = "true")
 @EnableConfigurationProperties(AliyunProperties.class)
 public class AliyunAutoConfigure {
 
@@ -37,21 +31,6 @@ public class AliyunAutoConfigure {
     @RefreshScope
     public AliyunSendHandler aliyunSendHandler(AliyunProperties properties, ObjectMapper objectMapper) {
         return new AliyunSendHandler(properties, objectMapper);
-    }
-
-    /**
-     * 启用判断
-     *
-     * @author guer
-     *
-     */
-    public static class EnableCondition implements Condition {
-
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return context.getEnvironment().getProperty("sms.aliyun.enable", Boolean.TYPE, true);
-        }
-
     }
 
 }
